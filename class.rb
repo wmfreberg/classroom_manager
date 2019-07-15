b2b1fb03f3e9da91284a60850c6d1a3e1e665ad8
Whitney Freberg <wmfreberg@gmail.com>
2019-07-15 18:57:06 -0400

    Added second paragraph

--- class.rb
+++ class.rb
@@ -1,3 +1,7 @@
 puts "This is the classroom manager"
 puts "It's just an example to get Git info started"
 puts "It doesn't really do a whole lot."
+
+puts "Here's a second paragraph"
+puts "Really, it's useless"
+puts "I'm just trying to put something here"
